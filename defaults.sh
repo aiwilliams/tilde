043c3d6b9acf682dba7757380509ea01e8f3ba4c
Adam Williams <adam@thewilliams.ws>
2011-01-03 10:59:09 -0500

    Moved some of my git configuration in, added a defaults.sh script for modifications to OS X defaults.

---
+++ defaults.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+killall Finder
--- setup.sh
+++ setup.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 ln -sv $PWD/lib/ $HOME/lib
+
+
+#######################
+#  ACK
+#######################
+ln -sfv $PWD/.ackrc $HOME/.ackrc
+
+
+#######################
+#  GIT
+#######################
+
+# A couple of things for Git that we want to be user global. Note that we
+# should NOT link .git and .gitmodules as these belong to this project and are
+# not intended to be in my user directory.
+ln -sfv $PWD/.gitconfig $HOME/.gitconfig
+ln -sfv $PWD/.gitignore $HOME/.gitignore
+ln -sfv $PWD/.gitusers $HOME/.gitusers
+
+
+#######################
+#  VIM
+#######################
 ln -sv $PWD/.vim/ $HOME/.vim
-ln -sv $PWD/.vimrc $HOME/.vimrc
-ln -sv $PWD/.gvimrc $HOME/.gvimrc
+ln -sfv $PWD/.vimrc $HOME/.vimrc
+ln -sfv $PWD/.gvimrc $HOME/.gvimrc
+
+# Pull in our .vim/bundles
 git submodule update --init
